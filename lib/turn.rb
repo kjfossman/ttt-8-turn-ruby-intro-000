047476ea5a34995e9f0ec8fc9f99978e0bd03fec
Kyle Fossman <kjfossman@gmail.com>
2020-12-22 16:05:29 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  puts "Where would you like to go?"
+  (input.to_i) - 1
+end
+
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+def valid_move?
+
+  if position_taken?(board, index) == true || index.between?(0,8) == false
+    false
+  else
+    true
+  end
+
+end
+
+def position_taken?(board, index)
+  
+  if board[index] == " " || board[index] == "" || board[index] ==  nil
+  false
+else
+  true
+end
+end
