b607cc54cb81fc1d130cd5bb05a0ff8cc9562f34
Kyle Fossman <kjfossman@gmail.com>
2020-12-22 16:03:05 +0000

    Automatically backed up by Learn

---
+++ lib/move.rb
@@ -0,0 +1,18 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  puts "Where would you like to go?"
+  (input.to_i) - 1
+end
+
+
+def move(board, index, value = "X")
+  board[index] = value
+end
