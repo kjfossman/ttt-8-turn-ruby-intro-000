92fd27616adf5d4be757649f2a35b726b65c92df
Kyle Fossman <kjfossman@gmail.com>
2020-12-22 17:29:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,10 @@ end
 
 
 def turn(board)
-
+  puts "Please enter 1-9:"
+input = gets.strip.to_i
+input_to_index(input)
+index = input - 1
 until valid_move?(board, index) == true
   puts "Please enter 1-9:"
 input = gets.strip.to_i
